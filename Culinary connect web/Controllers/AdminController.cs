1a483a4920d1453699a639baee097590d27ae000
daniel petcov <danielpetcov.work@gmail.com>
2025-05-11 23:23:03 +0300

    updated the front end and crud operations for users inside the admin dashboard

--- Culinary connect web/Controllers/AdminController.cs
+++ Culinary connect web/Controllers/AdminController.cs
@@ -3,11 +3,13 @@ using System.Linq;
 using System.Web.Helpers;
 using System.Web.Mvc;
 using culinaryConnect.BusinessLogic.Data;
+using culinaryConnect.BusinessLogic.Models.UserDB;
 using culinaryConnect.Domain.Entities.Admin;
 using culinaryConnect.Web.Services.AdminService;
 using culinaryConnect.Domain.Entities.Category;
 using culinaryConnect.BusinessLogic.Models;
 using System.Data.Entity.Migrations;
+using culinaryConnect.Domain.Entities.User;
 
 namespace Culinary_connect_web.Controllers
 {
@@ -15,7 +17,7 @@ namespace Culinary_connect_web.Controllers
     {
         private readonly CulinaryContext _context = new CulinaryContext();
 
-        // GET: Admin
+        // Pages
         public ActionResult Index()
         {
             if (Session["AdminID"] != null) { 
@@ -39,7 +41,77 @@ namespace Culinary_connect_web.Controllers
 
         public ActionResult Login()
         {
+            if (Session["AdminID"] != null)
+            {
+                return RedirectToAction("index");
+            }
             return View();
+        }
+
+        public ActionResult Users(UsersPageModel model)
+        {
+            if (Session["AdminID"] == null)
+            {
+                return RedirectToAction("login");
+            }
+
+            var users = _context.Users.ToList().Select(u => new User {
+                Id = u.Id,
+                Name = u.UserName,
+                Email = u.UserEmail,
+                SubscribedNews = u.SubscribedToNews,
+            }).ToList();
+            model.Users = users;
+            return View(model);
+        }
+
+        public ActionResult Recipes()
+        {
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+            return View();
+        }
+
+        public ActionResult Reports()
+        {
+            return View();
+        }
+
+
+        public ActionResult Category()
+        {
+            if (Session["AdminID"] == null)
+            {
+                return RedirectToAction("login");
+            }
+
+            var categories = _context.Categories.ToList().Select(c => new Category
+            {
+                Id = c.Id,
+                Title = c.Title,
+                RecipesID = c.Recipies
+            }).ToList();
+
+            var model = new CategoryPageModel
+            {
+                FormCategory = new CategoryForm(),
+                Categories = categories
+            };
+
+            return View(model);
+        }
+
+
+        // Functions
+        [HttpPost]
+        public ActionResult Logout() {
+            Session["AdminID"] = null;
+            Session["AdminEmail"] = null;
+
+            return RedirectToAction("login");
         }
 
         [HttpPost]
@@ -67,28 +139,92 @@ namespace Culinary_connect_web.Controllers
             return View("login");
         }
 
-        public ActionResult Category()
+        [HttpPost]
+        public ActionResult CreateUser(UsersPageModel model)
         {
             if (Session["AdminID"] == null)
             {
                 return RedirectToAction("login");
             }
 
-            var categories = _context.Categories.ToList().Select(c => new Category
+            var userModel = model.UserRegisterModel;
+            var existingUser = _context.Users.FirstOrDefault(u => u.UserEmail == userModel.UserEmail);
+            if (existingUser != null)
             {
-                Id = c.Id,
-                Title = c.Title,
-                RecipesID = c.Recipies
-            }).ToList();
+                ViewBag.ErrorMessage = "Such a user already exists";
+                model.UserRegisterModel = new UserRegisterModel();
+                return View("Users", model);
+            }
 
-            var model = new CategoryPageModel
+            var userPasswordHash = Crypto.SHA256(userModel.UserPassword + "tralalero");
+            _context.Users.Add(new UserDB
             {
-                FormCategory = new CategoryForm(),
-                Categories = categories
-            };
+                UserEmail = userModel.UserEmail,
+                UserName = userModel.UserName,
+                PasswordHash = userPasswordHash,
+            });
 
-            return View(model);
+            _context.SaveChanges();
+            model.UserRegisterModel = new UserRegisterModel();
+            return RedirectToAction("Users",model);
         }
+
+        [HttpPost]
+        public ActionResult EditUser(UsersPageModel model) {
+            if (Session["AdminID"] == null)
+            {
+                return RedirectToAction("login");
+            }
+
+            var userModel = model.UserEditModel;
+
+            var user = _context.Users.FirstOrDefault(u => u.Id == userModel.Id);
+            if(user == null)
+            {
+                ViewBag.ErrorMessage = "There is no such user" + userModel.Id;
+                model.UserEditModel = new UserEditModel();
+                model.Users = _context.Users.ToList().Select(u => new User
+                {
+                    Id = u.Id,
+                    Email = u.UserEmail,
+                    Name = u.UserName,
+                    SubscribedNews = u.SubscribedToNews
+                }).ToList();
+                return View("users", model);
+            }
+
+            user.UserName = userModel.Name;
+            user.UserEmail = userModel.Email;
+            if(userModel.Email.Length != 0) {
+                user.PasswordHash = Crypto.SHA256(userModel.Password + "tralalero");
+            }
+            _context.SaveChanges();
+
+
+            return RedirectToAction("users");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteUser(UsersPageModel model) {
+            if (Session["AdminID"] == null)
+            {
+                return RedirectToAction("login");
+            }
+
+            var userModel = model.UserDeleteModel;
+            var existingUser = _context.Users.FirstOrDefault(u => u.Id ==  userModel.Id);
+            if(existingUser == null)
+            {
+                ViewBag.ErrorMessage = "There is no such user";
+                model.UserDeleteModel = new UserDeleteModel();
+                return View("users",model);
+            }
+
+            _context.Users.Remove(existingUser);
+            _context.SaveChanges();
+            return RedirectToAction("users");
+        }
+
 
         [HttpPost]
         public ActionResult AddCategory(CategoryPageModel model)
